d21fea4e7a794bf58628701fe32ee36b43eb4ee5
Eva Morrison <eva.morrison33@ku.edu>
2021-04-28 18:11:05 -0500

    connect php to mysql

--- CreateUser.php
+++ CreateUser.php
@@ -1,5 +1,22 @@
 <?php
     $userID = $_POST["user_id"];
 
+    $mysqli = new mysqli("mysql.eecs.ku.edu", "evamorrison", "aipa9The", "evamorrison");
+
+    /* check connection */
+    if ($mysqli->connect_errno) {
+        printf("Connect failed: %s\n", $mysqli->connect_error);
+        exit();
+    }
+
+    $query = "SELECT * FROM Users";
+
+    if($userID=="")
+    {
+        echo '<script>alert("Invalid User ID\nUnable to add user to database")</script>';
+    }
+
     echo "User ID: " . $userID . "<br>";
+
+    $mysqli->close();
 ?>
