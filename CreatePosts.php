ea7b8227ed1b40a56d0c6b98249972d507b02bde
Eva Morrison <eva.morrison33@ku.edu>
2021-05-02 16:31:41 -0500

    create posts update

--- CreatePosts.php
+++ CreatePosts.php
@@ -1,5 +1,6 @@
 <?php
     $userID = $_POST["user_id"];
+    $post = $_POST["post"];
 
     $mysqli = new mysqli("mysql.eecs.ku.edu", "evamorrison", "aipa9The", "evamorrison");
 
@@ -9,18 +10,27 @@
         exit();
     }
 
-    $query = "INSERT INTO Users (user_id) VALUES ('$userID')";
-    $result = $mysqli->query($query);
+    $query2 = "SELECT EXISTS(SELECT usesr_id FROM Users WHERE user_id = '$userID')";
+    
 
-    if($userID==""||!($result))
+    if($userID==""||$post =="")
     {
-        echo '<script>alert("Invalid User ID\nUnable to add user to database")</script>';
+        echo '<script>alert("Invalid Input\nPlease fill all fields")</script>';
     }
     else{
-        echo "New user successfully added.<br>";
+        $result = $mysqli->query($query);
+        $result2 = $mysqli->query($query);
+        if(!($result2)){
+            echo '<script>alert("This user does not exist\nCreate a new user")</script>';
+        }
+        else{
+            echo "New post successfully added.<br>";
+        }
+        
     }
 
     echo "User ID: " . $userID . "<br>";
+    echo "Post: " . $post . "<br>";
 
     $mysqli->close();
 ?>
